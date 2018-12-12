1a6dedc47ec5e5730726dc3abc47d9e773035b14
Zhida Zhang <zhangzhida822@outlook.com>
2018-12-12 16:56:26 -0500

    rpc, add favorite itemids to return

--- src/rpcHandler/SearchItem.java
+++ src/rpcHandler/SearchItem.java
@@ -2,6 +2,7 @@ package rpcHandler;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Set;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,11 +11,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
 
 import dbClient.DBConnection;
 import dbClient.DBConnectionFactory;
 import entity.Item;
-import ticketMasterClient.TicketMasterClient;
 
 
 /**
@@ -43,13 +44,17 @@ public class SearchItem extends HttpServlet {
 		
 		// Do Get from the connection/database
 		String term = request.getParameter("term");
-		DBConnection connection = DBConnectionFactory.getConnection();
+		String userId = request.getParameter("user_id");
 		
+		DBConnection connection = DBConnectionFactory.getConnection();
 		try {
 			List<Item> items = connection.searchItems(lat, lon, term);
+			Set<String> favoriteItemIds = connection.getFavoriteItemIds(userId); // get favorite list
 			JSONArray array = new JSONArray();
 			for (Item item : items) {
-				array.put(item.toJSONObject());
+				JSONObject obj = item.toJSONObject();
+				obj.put("favorite", favoriteItemIds.contains(item.getItemId()));
+				array.put(obj);
 			}
 			
 			RpcHelper.writeJSONArray(response, array);
